031c9e0233ddd342a62486e7e53332b68d1d2f96
xiaoyu_wu <xiaoyu_wu@foxitsoftware.cn>
2021-02-25 00:28:45 +0800

    feat:2-25

--- 1-YouDon'tKnowJS/mid/1-1.js
+++ 1-YouDon'tKnowJS/mid/1-1.js
@@ -39,5 +39,37 @@
     length
 }
 {
-    
+    Number.EPSILON ;//比较两个数字相等
+    void 0//并不影响表达式的结果 只是不返回值
+    Number.isNaN()
+    0 
+        Number(n) === 0 && 1/Number(0) === -Infinity
+    Object.is(a,b) //判断两值是否绝对性相等
+
+
+}
+
+{
+    function foo(x){
+        x.push(x);
+        console.log(x);
+        x = [4,5,6];
+        x.push(7)
+        console.log(x)
+    }
+    var a = [1,2,3];
+    foo(x);
+    a // [1,2,3,4]
+}
+{
+    function foo(x){
+        x.push(x);
+        console.log(x);
+        x.length = 0;
+        x.push(4,5,6,7)
+        console.log(x)
+    }
+    var a = [1,2,3];
+    foo(x);
+    a // [4,5,6,7]
 }
---
+++ 1-YouDon'tKnowJS/mid/1-3.js
@@ -0,0 +1,31 @@
+{
+    Object()
+    Array()
+    Function()
+    Number()
+    String()
+    Boolean()
+    Date()
+    Symbol()
+
+    *Error()
+    *RegExp()
+}
+{
+    内部属性
+        [[Class]]
+    ES5 
+    ES6
+        toString()
+        [[Class]]
+            行为有些许不同
+    得到封装对象的基本类型值
+        valueOf()
+    Date.now() // 时间戳
+
+    Symbol 
+        符号是具有唯一性的特殊值(并非绝对)
+}
+{
+    46
+}
