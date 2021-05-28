39e06ced2f3b5576b0f97d2078c47fa717774ca2
xiaoyu_wu <xiaoyu_wu@foxitsoftware.cn>
2021-05-28 18:17:49 +0800

    feat:5-28

---
+++ Questions/Q/exmaple.js
@@ -0,0 +1,38 @@
+{
+  "cDocIDList": ["https://cws-fz01.connectedpdf.com/cDocID/F5B462BA-A81B-4B05-E61E-FD580200203B"],
+  "fileLink": "",
+  "notice": "0",
+  "invitation": "0",
+  "userList": [{
+  "userEmail": "",
+  "emailDomain": "",
+  "isGroup": 0,
+  "everyone": 1,
+  "permitType": 4,
+  "fullControl": "0",
+  "permission": [210, 128, 804],
+  "offline": 0,
+  "offlineDays": 0,
+  "online": 0,
+  "deviceLimit": 0,
+  "accessPages":
+  
+  { "isAccessPages": 0, "wrapperContent": "您没有查看这些页面的权限", "wrapperPic": "", "Pages": [] }
+  ,
+  "deviceAccess":
+  
+  { "ip": [ ["192.16.68.3", "192.168.68.15"], "192.16.40.3","192.16.50.30" ], "mac": ["18:81:0e:ee:e5:60", "18:81:0e:ee:a0:a8", "18:81:0e:ee:x0:55"] }
+  ,
+  "printAccess":
+  
+  { "printCount": 5, "printExpireTime": "2020-10-25 05:25:00" }
+  ,
+  "viewAccess":
+  
+  { "viewCount": 8, "viewDuration": [ ["2020-10-25 05:25:00", "2020-11-25 05:25:00"] ] }
+  ,
+  "accessPagesStr": "",
+  "onlineDays": 0
+  }],
+  "lang": "zh-CN"
+  }
